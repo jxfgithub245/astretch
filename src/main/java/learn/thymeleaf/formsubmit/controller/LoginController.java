32b70a3cab180cd01f6fffff8c505c208bba7baf
jxfgithub245 <15000918245@sina.cn>
2020-09-01 22:37:23 +0800

    'thymeleafform'

---
+++ src/main/java/learn/thymeleaf/formsubmit/controller/LoginController.java
@@ -0,0 +1,22 @@
+package learn.thymeleaf.formsubmit.controller;
+
+import learn.thymeleaf.formsubmit.model.LoginBean;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("/toLogin")
+    public String toLogin(Model model){
+        model.addAttribute("loginBean",new LoginBean());
+        return "login";
+    }
+
+    @RequestMapping("/login")
+    public String greetingSubmit(@ModelAttribute LoginBean loginbean){
+        System.out.println("测试提交的数据"+loginbean.getUname());
+        return "result";
+    }
+}
---
+++ src/main/java/learn/thymeleaf/formsubmit/controller/SpringBootApplicationTest2.java
@@ -0,0 +1,14 @@
+package learn.thymeleaf.formsubmit.controller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringBootApplicationTest2 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootApplicationTest2.class, args);
+    }
+}
