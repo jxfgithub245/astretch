323e14ed5805ad328037b2fde6f46f3eca1fae64
jxfgithub245 <15000918245@sina.cn>
2020-09-16 23:02:04 +0800

    't'

--- src/test/java/groups/Test1.java
+++ src/test/java/groups/Test1.java
@@ -1,7 +1,11 @@
 package groups;
 
 
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
+import io.qameta.allure.Link;
+import io.qameta.allure.Issue;
+import io.qameta.allure.TmsLink;
 
 /*
  *包名:com.kdzwy.practice
@@ -11,16 +15,26 @@ import org.testng.annotations.Test;
  **/
 public class Test1 {
     @Test(groups = { "functest", "checkintest" })
+    @Link("https://example.org")
+    @Link(name = "allure", type = "mylink")
+    @Story("Base support for bdd annotations")
     public void testMethod1() {
         System.out.println("method1checkintest");
     }
 
     @Test(groups = {"functest", "checkintest"} )
+    @Issue("123")
+    @Issue("432")
+    @Story("Advanced support for bdd annotations")
     public void testMethod2() {
         System.out.println("method2checkintest");
     }
 
     @Test(groups = { "functest" })
+    @TmsLink("test-1")
+    @TmsLink("test-2")
+    @Story("Base support for bdd annotations")
+    @Story("Advanced support for bdd annotations")
     public void testMethod3() {
         System.out.println("method3functest");
     }
